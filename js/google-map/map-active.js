062a576b93fd7623073c229941285129ab3d6e31
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2022-10-12 13:02:30 +0100

    Add files via upload

---
+++ js/google-map/map-active.js
@@ -0,0 +1,39 @@
+var map;
+var latlng = new google.maps.LatLng(-8.850963, 13.233064);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+            }]
+        }];
+var mapOptions = {
+    zoom: 15,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var geocoder_map = new google.maps.Geocoder();
+var address = 'Luanda';
+geocoder_map.geocode({
+    'address': address
+}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: map.getCenter()
+        });
+    } else {
+        alert("O geocódigo não foi bem-sucedido pelo seguinte motivo: " + status);
+    }
+});
+var mapType = new google.maps.StyledMapType(stylez, {
+    name: "Grayscale"
+});
+map.mapTypes.set('gMap', mapType);
+map.setMapTypeId('gMap');
